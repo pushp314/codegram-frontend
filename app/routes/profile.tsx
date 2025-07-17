821ec99ad80dbe72122d17888776cdc53ca5f3ae
PUSHP RAJ SHARMA <98100352+pushp314@users.noreply.github.com>
2025-07-18 02:58:57 +0530

    feat: Remove deprecated profile routes and related components

--- app/routes/profile.tsx
+++
@@ -1,9 +0,0 @@
-import { LoaderFunctionArgs, redirect } from "@remix-run/node";
-import { requireAuth } from "~/utils/auth.server";
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  const user = await requireAuth(request);
-  
-  // Redirect to the user's profile page
-  return redirect(`/profile/${user.username}`);
-}
